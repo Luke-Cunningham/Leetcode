102d4eba2ccd929dc90f5d0e3a4b0998b630c028
Lukas Cunningham <97056435+Luke-Cunningham@users.noreply.github.com>
2022-10-26 08:44:51 -0600

    problem 21

--- main.py
+++ main.py
@@ -122,6 +122,37 @@ class Solution3(object):
         return max_length
 
 
+#######################################################################################################################
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution21(object):
+    def mergeTwoLists(self, list1, list2):
+        """
+        :type list1: Optional[ListNode]
+        :type list2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        new_list = dummy = ListNode()
+
+        while list1 and list2:
+            if list1.val <= list2.val:
+                new_list.next = list1
+                list1, new_list = list1.next, list1
+
+            else:
+                new_list.next = list2
+                list2, new_list = list2.next, list2
+
+        if list1 or list2:
+            new_list.next = list1 if list1 else list2
+
+        return dummy.next
+
+
 if __name__ == '__main__':
     s = Solution3()
     print(s.lengthOfLongestSubstring("abbbbdvdf"))
